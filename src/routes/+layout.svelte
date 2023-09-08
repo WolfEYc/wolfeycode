d40397e1846ea22756400b0d7634cfd08f14c928
WolfEYc <isaac.wolf@live.com>
2023-09-08 17:04:11 -0500

    victory!

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+
+<header>
+	<nav class="container" aria-label="breadcrumb">
+		<ul>
+			<li><a href="/accounts">accounts</a></li>
+			<li><a href="/purchases">purchases</a></li>
+		</ul>
+	</nav>
+</header>
+<main class="container main">
+	<slot />
+</main>
+
+<style>
+	header {
+		overflow: hidden;
+		background-color: var(--sk-back-4);
+		position: fixed;
+		top: 0;
+		width: 100%; 
+	}
+
+	a:hover {
+		background: var(--primary-hover);
+		color: black;
+	}
+
+	.main {
+		margin-top: 6%;
+	}
+</style>
