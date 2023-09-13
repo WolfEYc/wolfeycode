5ca306599a6dc786851edcfa719c876edb833fd1
WolfEYc <isaac.wolf@live.com>
2023-09-13 13:51:23 -0500

    wups account

--- src/routes/(tracker)/account/+page.js
+++ src/routes/(tracker)/account/+page.js
@@ -1,18 +1,8 @@
+import { GetPage } from '../endpoint';
+
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch, url }) {
-	endpoint.search = url.search
-	endpoint.searchParams.set("lookahead", "1");
-	const res = await fetch(endpoint, { headers: { "Cache-Control": "max-age=3600" } });
-	const rows = await res.json();
-
-	return { rows };
+	url.searchParams.set("page", "0");
+	const rows = await GetPage(fetch, url);
+	return { eof: rows.eof, rows: rows.purchases };
 }
-
-
-/**
-* API docs https://purchase.wolfeycode.com/docs
-*/
-const dev = "http://127.0.0.1:8080/";
-const prod = "https://purchase.wolfeycode.com/";
-
-const endpoint = new URL(dev + "account")
